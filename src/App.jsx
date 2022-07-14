4e5a98382cb50e9ac5173663d141e57c8f6aad62
DiegoBelPe <dbeltranpereira@gmail.com>
2022-07-13 21:44:36 -0500

    add styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+
+const Heading = styled.h1`
+  font-family: 'lato', sans-serif;
+  color: #fff;
+
+`
 
 function App() {
 
   return (
-    <h1>Hola desde cripto</h1>
+    <Heading>Hola desde cripto</Heading>
   )
 }
 
