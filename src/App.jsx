251cb3e64d851d01c9102f9405a7ea0d8868fbf5
DiegoBelPe <dbeltranpereira@gmail.com>
2022-07-14 21:07:14 -0500

    feat: styles components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
+import ImageCripto from './assets/imagen-criptos.png'
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+
+`
+const Image = styled.img`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+`
 
 const Heading = styled.h1`
   font-family: 'lato', sans-serif;
@@ -10,7 +28,15 @@ const Heading = styled.h1`
 function App() {
 
   return (
-    <Heading>Hola desde cripto</Heading>
+    <Contenedor>
+    <Image 
+      src={ImageCripto} 
+      alt="imagen cripto"
+    />
+    <div>
+      <Heading>Cotizador de criptomonedas</Heading>
+    </div>
+    </Contenedor>
   )
 }
 
